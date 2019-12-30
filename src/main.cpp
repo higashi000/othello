06cb29bd0cbb59f0435dce2136a79c3b870c8360
higashi000 <higashi.ict@gmail.com>
2019-12-30 16:00:15 +0900

    Add game

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,33 @@ using namespace Othello;
 int main() {
   Field f;
 
-  f.dispField();
+  bool player = true;
+
+  while (1) {
+    f.searchPutPos(player);
+    f.dispField();
+
+    int num;
+    char playerPice = (player ? 'B' : 'W');
+    std::cout << "Now Player >> " << playerPice << std::endl << std::endl;
+    std::cout << "Please input your choice >>";
+    std::cin >> num;
+    f.reversePice(num - 1, player);
+
+
+    if (f.checkFinish()) {
+      auto point = f.calcPoint();
+
+      std::cout << "BLACK: " << point.first << std::endl;
+      std::cout << "WHITE: " << point.second << std::endl;
+
+      if (point.first > point.second) std::cout << "Winner: BLACK" << std::endl;
+      else if (point.first < point.second) std::cout << "Winner: WHITE" << std::endl;
+      else std::cout << "draw" << std::endl;
+
+      break;
+    }
+
+    player = !player;
+  }
 }
